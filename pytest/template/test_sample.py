dec152a5ab748f14ab3edf7c5256aac5f2e89a07
yz <yangzhen@sunmi.com>
2020-05-08 23:33:05 +0800

    我也不知道是啥，提交一把

--- pytest/template/test_sample.py
+++ pytest/template/test_sample.py
@@ -9,4 +9,30 @@ def test_answer():
     assert func(4) == 5
 
 if __name__ == "__main__":
-    pytest.main('-q test_sample.py')
+    # pytest.main('-q test_sample.py')
+    # 求和最趋近于10的几个值
+    '''
+	一堆数值求和，组合列表，求和
+    '''
+    a = [1, 5, 6, 3, 4, 7]
+    wanted = 10
+    wanted_list = []
+    print(sum(a))
+    tmp_list = [0]
+    for item in a:
+    	tmp_list.append(item)
+    	sum_wanted = sum(wanted_list)
+    	sum_tmp = sum(tmp_list)
+    	if sum_tmp == wanted:
+    		wanted_list = tmp_list
+    		break
+    	elif sum_tmp > wanted:
+    		if sum_wanted == 0:
+    			wanted_list = tmp_list
+    		elif sum_wanted < sum_tmp:
+    			tmp_list.pop(1)
+    		else:
+    			wanted_list = tmp_list
+    	else:
+    		continue
+    	print(tmp_list, wanted_list)
